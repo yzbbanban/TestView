96f9cfade60032e57aae67f364c60ec6fe282c3e
yzbbanban <yzbbanban@live.com>
2018-02-28 14:10:22 +0800

    动态设置进度

--- app/src/main/java/com/riti/testview/MainActivity.java
+++ app/src/main/java/com/riti/testview/MainActivity.java
@@ -2,14 +2,43 @@ package com.riti.testview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private WareView mWareView;
+    private int i;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        MyView myView=findViewById(R.id.my_view);
-        myView.setDate(270,90);
+//        MyView myView=findViewById(R.id.my_view);
+//        myView.setDate(270,90);
+        mWareView = findViewById(R.id.wv);
+
+        new Thread() {
+            @Override
+            public void run() {
+                super.run();
+                try {
+                    for (int i = 0; i < 100; i++) {
+                        Log.i(TAG, "run: "+i);
+                        MainActivity.this.i = i;
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                mWareView.setProcess(MainActivity.this.i);
+                            }
+                        });
+                        sleep(1000);
+                    }
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }.start();
+
     }
 }
--- app/src/main/java/com/riti/testview/WareView.java
+++ app/src/main/java/com/riti/testview/WareView.java
@@ -2,14 +2,18 @@ package com.riti.testview;
 
 import android.animation.ValueAnimator;
 import android.content.Context;
+import android.graphics.BlurMaskFilter;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
 import android.os.Build;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.LinearInterpolator;
 
@@ -19,7 +23,7 @@ import static android.graphics.Path.Op.INTERSECT;
  * Created by YZBbanban on 2018/2/28.
  */
 
-public class WareView extends View{
+public class WareView extends View {
     private Paint mPaint;
     private Path mPath;
     private float offset;//偏移量
@@ -42,17 +46,18 @@ public class WareView extends View{
     }
 
     private void init() {
-        mPaint=new Paint();
+        mPaint = new Paint();
         mPaint.setAntiAlias(true);
         mPaint.setDither(true);
-        mPath=new Path();
+        mPath = new Path();
 
     }
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
-        baseLine=MeasureSpec.getSize(heightMeasureSpec)/2;
+        baseLine = MeasureSpec.getSize(heightMeasureSpec) / 2 + 150;
+        Log.i(TAG, "onMeasure---->: " + baseLine);
     }
 
     @Override
@@ -65,43 +70,68 @@ public class WareView extends View{
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
+        float radius = 150.0f;
+//        Log.i(TAG, "onDraw: "+getWidth());
         mPaint.setColor(Color.YELLOW);
-        mPaint.setStyle(Paint.Style.FILL_AND_STROKE);
-        float x1=getWidth()/2;
-        float y1=getHeight()/2;
-        float x2=getWidth()/2;
-        float y2=getHeight();
+        mPaint.setStyle(Paint.Style.FILL);
+        int layerId = canvas.saveLayer(0, 0, getWidth(), getHeight(), null, Canvas.ALL_SAVE_FLAG);
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, radius, mPaint);
+
+        mPaint.setColor(Color.YELLOW);
+        mPaint.setStyle(Paint.Style.FILL);
+        mPaint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.CLEAR));
         mPath.reset();
-        mPath.moveTo(-getWidth()+offset,baseLine);
-        mPath.quadTo(-3*getWidth()/4+offset,baseLine-100,-getWidth()/2+offset,baseLine);
-        mPath.quadTo(-1*getWidth()/4+offset,baseLine+100,offset,baseLine);
-        mPath.quadTo(1*getWidth()/4+offset,baseLine-100,getWidth()/2+offset,baseLine);
-        mPath.quadTo(3*getWidth()/4+offset,baseLine+100,getWidth()+offset,baseLine);
+        mPath.moveTo(-getWidth() + offset, baseLine);
 
+        int wareStep = 8;
+        for (int i = 0; i < wareStep; i++) {
+            float base = baseLine;
+            if (i % 2 == 0) {
+                base = base + 30f;
+            } else {
+                base = base - 30f;
+            }
+            mPath.quadTo(
+                    (2 * i - wareStep + 1) * getWidth() / wareStep + offset,
+                    base,
+                    2 * (i - 3) * getWidth() / wareStep + offset,
+                    baseLine);
+        }
 
-        mPath.lineTo(getWidth(),getHeight());
-        mPath.lineTo(-getWidth()+offset,getHeight());
+//        mPath.quadTo(-7 * getWidth() / 8 + offset, baseLine - 30, -3*getWidth() / 4 + offset, baseLine);
+//        mPath.quadTo(-5 * getWidth() / 8 + offset, baseLine + 30, -getWidth()/2+offset, baseLine);
+//        mPath.quadTo(-3 * getWidth() / 8 + offset, baseLine - 30, -getWidth() / 4 + offset, baseLine);
+//        mPath.quadTo(-1 * getWidth() / 8 + offset, baseLine + 30, offset, baseLine);
+//
+//        mPath.quadTo(1 * getWidth() / 8 + offset, baseLine - 30, getWidth() / 4 + offset, baseLine);
+//        mPath.quadTo(3 * getWidth() / 8 + offset, baseLine + 30, getWidth()/2 + offset, baseLine);
+//        mPath.quadTo(5 * getWidth() / 8 + offset, baseLine - 30, 3*getWidth() / 4 + offset, baseLine);
+//        mPath.quadTo(7 * getWidth() / 8 + offset, baseLine + 30, getWidth() + offset, baseLine);
+
+        mPath.lineTo(getWidth(), 0.0f);
+        mPath.lineTo(-getWidth() + offset, 0.0f);
         mPath.close();
 
-        float radius=100.0f;
-        Path pathCircle=new Path();
-        pathCircle.addCircle(getWidth()/2,getHeight()/2,radius, Path.Direction.CCW);
-        mPath.op(mPath,pathCircle,INTERSECT);
-        canvas.drawPath(mPath,mPaint);
 
-        mPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawPath(mPath, mPaint);
+
+        mPaint.setXfermode(null);
+        canvas.restoreToCount(layerId);
+
         mPaint.setColor(Color.BLACK);
-        canvas.drawCircle(getWidth()/2,getHeight()/2,radius,mPaint);
+        mPaint.setStyle(Paint.Style.STROKE);
+        canvas.drawCircle(getWidth() / 2, getHeight() / 2, radius, mPaint);
     }
 
-    private void xController(){
-        ValueAnimator mAnimator = ValueAnimator.ofFloat(0,getWidth());
+    private void xController() {
+        ValueAnimator mAnimator = ValueAnimator.ofFloat(0, getWidth());
         mAnimator.setInterpolator(new LinearInterpolator());
         mAnimator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
             @Override
             public void onAnimationUpdate(ValueAnimator valueAnimator) {
-                float animatorValue = (float)valueAnimator.getAnimatedValue() ;
-                offset =animatorValue;//不断的设置偏移量，并重画
+                float animatorValue = (float) valueAnimator.getAnimatedValue();
+                offset = animatorValue;//不断的设置偏移量，并重画
+//                baseLine =animatorValue;//不断的设置偏移量，并重画
                 postInvalidate();
 
             }
@@ -109,5 +139,19 @@ public class WareView extends View{
         mAnimator.setDuration(1000);
         mAnimator.setRepeatCount(ValueAnimator.INFINITE);
         mAnimator.start();
+    }
+
+    private static final String TAG = "WareView";
+
+    public void setProcess(int process) {
+        Log.i(TAG, "setProcess: " + process);
+        //[-150,150]
+        float step = process * 3f;
+
+        float pro = getHeight() / 2 + (150f - step);
+        Log.i(TAG, "setProcess getWidth: " + getWidth());
+        Log.i(TAG, "pro: " + pro);
+
+        this.baseLine = pro;
     }
 }
